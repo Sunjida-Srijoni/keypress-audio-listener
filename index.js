7be12c282dfd6a4e0474ff2e94c05711c379eb13
Sunjida-Srijoni <sunjidasrijoni@gmail.com>
2024-02-16 23:45:21 +0600

    audio has been added with keypress

--- index.js
+++ index.js
@@ -9,20 +9,29 @@ for(var i = 0; i<3 ; i++)
         console.log(text);
 
 
-        switch(text){
-            case "a":
-                var audio = new Audio("a.mp3");
-                audio.play();
-                break;
-            case "b":
-                var audio = new Audio("b.mp3");
-                audio.play();
-                break;
-            case "c":
-                var audio = new Audio("c.mp3");
-                audio.play();
-                break;
-        }
+       //audioPlay(text);
 
    });
+}
+
+document.addEventListener("keypress", function (e){
+    var text = e.key;
+    audioPlay(text);
+})
+
+function audioPlay(text){
+    switch(text){
+        case "a":
+            var audio = new Audio("a.mp3");
+            audio.play();
+            break;
+        case "b":
+            var audio = new Audio("b.mp3");
+            audio.play();
+            break;
+        case "c":
+            var audio = new Audio("c.mp3");
+            audio.play();
+            break;
+    }
 }
